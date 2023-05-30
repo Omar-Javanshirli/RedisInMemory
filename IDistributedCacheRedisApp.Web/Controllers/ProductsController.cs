b621a6bec8badb2a5b2d73896006f3df4f1b127d
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-05-30 02:56:18 -0700

    Add ImageCache and ImageUri function in productController

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.VisualBasic;
 using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -35,14 +36,14 @@ namespace IDistributedCacheRedisApp.Web.Controllers
 
             Product product = new Product { Id = 1, Name = "Pencil", Price = 10 };
             string jsonProduct = JsonConvert.SerializeObject(product);
-            await this.distributedCache.SetStringAsync("product:1", jsonProduct,cacheOptions)!;
+            await this.distributedCache.SetStringAsync("product:1", jsonProduct, cacheOptions)!;
             return View();
         }
 
         public async Task<IActionResult> ShowJsonProductSerialize()
         {
             string jsonProduct = await this.distributedCache.GetStringAsync("product:1");
-            Product p=JsonConvert.DeserializeObject<Product>(jsonProduct);
+            Product p = JsonConvert.DeserializeObject<Product>(jsonProduct);
             ViewBag.Product = p;
             return View();
         }
@@ -63,13 +64,14 @@ namespace IDistributedCacheRedisApp.Web.Controllers
 
         public async Task<IActionResult> ShowProductSerializeByBinariy()
         {
-            Byte[] byteProduct =await this.distributedCache.GetAsync("product:1");
-            string jsonProduct =Encoding.UTF8.GetString(byteProduct);
-            
-            Product p= JsonConvert.DeserializeObject<Product>(jsonProduct);
+            Byte[] byteProduct = await this.distributedCache.GetAsync("product:1");
+            string jsonProduct = Encoding.UTF8.GetString(byteProduct);
+
+            Product p = JsonConvert.DeserializeObject<Product>(jsonProduct);
             ViewBag.Product = p;
             return View();
         }
+
         public async Task<IActionResult> Show()
         {
             var name = await this.distributedCache.GetStringAsync("name");
@@ -81,6 +83,22 @@ namespace IDistributedCacheRedisApp.Web.Controllers
         {
             await this.distributedCache.RemoveAsync("name");
             return View();
+        }
+
+        public IActionResult ImageCache()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images/omer.png");
+
+            byte[] imageByte = System.IO.File.ReadAllBytes(path);
+            this.distributedCache.Set("resim",imageByte);
+
+            return View();
+        }
+
+        public IActionResult ImageUri()
+        {
+            Byte[] imageByte = this.distributedCache.Get("resim");
+            return File(imageByte, "image/png");
         }
     }
 }
