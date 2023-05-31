821250efc906afa443e7d3e9353bd1dcf2c9aa4e
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-05-31 01:16:27 -0700

    Add ListTypeController in RedisExhangeAPI.Web

---
+++ RedisExchangeAPI.Web/Controllers/ListTypeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Servicies;
+using StackExchange.Redis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class ListTypeController : Controller
+    {
+        private readonly RedisService redisService;
+        private readonly IDatabase db;
+        private string listkey = "names";
+
+        public ListTypeController(RedisService redisService)
+        {
+            this.redisService = redisService;
+            this.db = this.redisService.GetDb(1);
+        }
+
+        public IActionResult Index()
+        {
+            LinkedList<string> nameList = new LinkedList<string>();
+
+            if (this.db.KeyExists(listkey))
+            {
+                this.db.ListRange(listkey).ToList().ForEach(x =>
+                {
+                    nameList.AddLast(x.ToString());
+                });
+            }
+
+            return View(nameList);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            this.db.ListRightPush(listkey, name);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Remove(string name)
+        {
+            await this.db.ListRemoveAsync(listkey, name);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteFirstItem()
+        {
+            db.ListLeftPop(listkey);
+            return RedirectToAction("Index");
+        }
+    }
+}
